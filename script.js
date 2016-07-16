0a583aa9171ee42f7f25174b53da5027ae50f3af
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-16 03:39:56 -0400

    Added event handlers and Listeners, and requestAnimationFrame

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2; //define x and y starting point bottom center
+var y = (canvas.height-(canvas.height*0.5));
+var rightPressed = false; //left or right gets pressed?
+var leftPressed = false;
 
-//rectangle
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+//is the key held down or not?
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {  //right arrow key
+			rightPressed = true;
+	}
+	else if(e.keyCode == 37) {  //left arrow key
+			leftPressed = true;
+	}
+}
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {  //right key
+			rightPressed = false;
+	}
+	else if(e.keyCode == 37) {  //left key
+			leftPressed = false;
+	}
+}
+
+function drawRectangle() {
 	ctx.beginPath();
 	ctx.rect(20, 40, 50, 50); //(topleftX, topleftY, width, height)
 	ctx.fillStyle = "#FF0000";
 	ctx.fill();
 	ctx.closePath();
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawRectangle();
+
+	if(rightPressed){
+		console.log("Right Was Pressed!");
+	}
+	else if (leftPressed){
+		console.log("Left Was Pressed!");
+	}
+requestAnimationFrame(draw); //sets the interval frame rate to browser automated value
+}
+
+draw()
