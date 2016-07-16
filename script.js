27ea9affdfaf690b2a821f8b8ec37e864d05dd47
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-16 16:56:52 -0400

    The boat flips direction depending on the last key pressed

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var x = canvas.width/10; //define x and y starting point
+var x = canvas.width/4; //define x and y starting point
 var y = canvas.height/10;
 
 var rightPressed = false; //left or right gets pressed?
 var leftPressed = false;
+var leftLast = false;
+var rightLast = true;
 var speed = 5;
 //images
 var boat = document.createElement('img'); // DOM HTMLImageElement
 boat.src = 'images/boat.png';
 boat.alt = 'boat';
+
+var boatLeft = document.createElement('img'); // DOM HTMLImageElement
+boatLeft.src = 'images/boat_left.png';
+boatLeft.alt = 'boatLeft';
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -33,33 +39,37 @@ function keyUpHandler(e) {
 }
 
 function drawBoat() {
-	// ctx.beginPath();
-	// ctx.rect(x, y, 50, 50); //(topleftX, topleftY, width, height)
-	// ctx.fillStyle = "#FF0000";
-	// ctx.fill();
-
-	// document.body.appendChild(boat);
 	ctx.drawImage(boat,x,y);
-
-	ctx.closePath();
 }
-
-
+function drawBoatLeft() {
+	ctx.drawImage(boatLeft,x,y);
+}
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawBoat();
+
+	if(rightLast == true){
+		drawBoat();
+	}
+	else {
+		ctx.clearRect(x, y, boat.width, boat.height);
+		drawBoatLeft();
+	}
 
 	if(rightPressed){
 		console.log("Right Was Pressed!");
+		rightLast = true;
+		//drawBoat();
 		x = x + speed;
-		//boat.x++;
 	}
 	else if (leftPressed){
 		console.log("Left Was Pressed!");
+		rightLast = false;
+		ctx.clearRect(x, y, boat.width, boat.height);
+		drawBoatLeft();
 		x = x - speed;
-		//boat.x--;
 	}
+
 requestAnimationFrame(draw); //sets the interval frame rate to browser automated value
 }
 
