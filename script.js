5711899ad80143881d19b5b3c55de8866b08125f
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-16 14:50:11 -0400

    got the boat on canvas and moving

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2; //define x and y starting point
-var y = (canvas.height-(canvas.height*0.5));
+var y = canvas.height/3.5;
 var rightPressed = false; //left or right gets pressed?
 var leftPressed = false;
+var speed = 5;
+//images
+var boat = document.createElement('img'); // DOM HTMLImageElement
+boat.src = 'images/boat.png';
+boat.alt = 'boat';
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -26,27 +31,36 @@ function keyUpHandler(e) {
 	}
 }
 
-function drawRectangle() {
+function drawBoat() {
 	ctx.beginPath();
 	ctx.rect(x, y, 50, 50); //(topleftX, topleftY, width, height)
 	ctx.fillStyle = "#FF0000";
 	ctx.fill();
+
+	// document.body.appendChild(boat);
+	ctx.drawImage(boat,x,y);
+
 	ctx.closePath();
 }
 
+
+
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawRectangle();
+	drawBoat();
 
 	if(rightPressed){
 		console.log("Right Was Pressed!");
-		x++;
+		x = x + speed;
+		//boat.x++;
 	}
 	else if (leftPressed){
 		console.log("Left Was Pressed!");
-		x--;
+		x = x - speed;
+		//boat.x--;
 	}
 requestAnimationFrame(draw); //sets the interval frame rate to browser automated value
 }
+
 
 draw()
