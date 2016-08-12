5128cee6ed689b65c47f1011517b199401da8ff8
Mitch Pateman <mitch_pateman@hotmail.com>
2016-08-12 00:21:52 -0400

    testing links

--- script.js
+++ script.js
@@ -219,29 +219,29 @@ function mouseDownHandler(event) {
       //ELSE IF LINKS FISH IS CAUGHT AND LINK IS PRESSED x 5
       else if (liHooked == true && linksY < 287.13) {
         if (clickX > 533 && clickX < 602 && clickY > 308 && clickY < 378) {
-          setTimeout(function(){document.location.href = "https://www.linkedin.com/in/MitchellPateman"},500);
+          setTimeout(function(){window.location.href = "https://www.linkedin.com/in/MitchellPateman"},500);
           //window.location.href = "https://www.linkedin.com/in/MitchellPateman";
           console.log("linkedInPressed");
         }
         else if (clickX > 533 && clickX < 602 && clickY > 390 && clickY < 456) {
-          setTimeout(function(){document.location.href = "http://www.twitter.com/MitchPateman"},500);
+          setTimeout(function(){window.location.href = "http://www.twitter.com/MitchPateman"},500);
           //window.location.href = 'http://www.twitter.com/MitchPateman';
           console.log("twitterPressed");
         }
         else if (clickX > 533 && clickX < 602 && clickY > 468 && clickY < 533) {
-          setTimeout(function(){document.location.href = "http://www.github.com/MitchPateman"},500);
+          setTimeout(function(){window.location.href = "http://www.github.com/MitchPateman"},500);
 
           //window.location.href = 'http://www.github.com/MitchPateman';
           console.log("githubPressed");
         }
         else if (clickX > 533 && clickX < 602 && clickY > 543 && clickY < 611) {
           //window.location.href = "mailto:mitchell.pateman@gmail.com";
-          setTimeout(function(){document.location.href = "mailto:mitchell.pateman@gmail.com"},500);
+          setTimeout(function(){window.location.href = "mailto:mitchell.pateman@gmail.com"},500);
 
           console.log("emailPressed");
         }
         else if (clickX > 533 && clickX < 602 && clickY > 619 && clickY < 692) {
-          setTimeout(function(){document.location.href = "https://bitmakerhq.s3.amazonaws.com/alexa/production/student/resume/1077/mitch_pateman_resume_FINAL.pdf"},500);
+          setTimeout(function(){window.location.href = "https://bitmakerhq.s3.amazonaws.com/alexa/production/student/resume/1077/mitch_pateman_resume_FINAL.pdf"},500);
 
           //window.location.href = "https://bitmakerhq.s3.amazonaws.com/alexa/production/student/resume/1077/mitch_pateman_resume_FINAL.pdf";
           console.log("pdfPressed");
